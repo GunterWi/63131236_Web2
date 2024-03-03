686c34e7af7d0504c1b2420507022fe7f34f1b4d
GunterWi <boi09456@gmail.com>
2024-03-04 03:00:26 +0700

    SpringMVCMaven

---
+++ SpringMVCBai1Maven/src/main/java/jp/ivs/controller/WelcomeController.java
@@ -0,0 +1,12 @@
+package jp.ivs.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class WelcomeController {
+	@RequestMapping("/welcome")
+	public String welcome() {
+		return "welcome";
+	}
+}
