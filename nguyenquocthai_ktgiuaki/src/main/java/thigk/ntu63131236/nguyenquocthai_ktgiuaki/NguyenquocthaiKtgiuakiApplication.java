bd363e859938842b9158ed234b997f9e3ac47c6f
GunterWi <boi09456@gmail.com>
2024-03-26 15:09:43 +0700

    Upload Giuaky

---
+++ nguyenquocthai_ktgiuaki/src/main/java/thigk/ntu63131236/nguyenquocthai_ktgiuaki/NguyenquocthaiKtgiuakiApplication.java
@@ -0,0 +1,13 @@
+package thigk.ntu63131236.nguyenquocthai_ktgiuaki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NguyenquocthaiKtgiuakiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NguyenquocthaiKtgiuakiApplication.class, args);
+	}
+
+}
---
+++ nguyenquocthai_ktgiuaki/src/test/java/thigk/ntu63131236/nguyenquocthai_ktgiuaki/NguyenquocthaiKtgiuakiApplicationTests.java
@@ -0,0 +1,13 @@
+package thigk.ntu63131236.nguyenquocthai_ktgiuaki;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NguyenquocthaiKtgiuakiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
