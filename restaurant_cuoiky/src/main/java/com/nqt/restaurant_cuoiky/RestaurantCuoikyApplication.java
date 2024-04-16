5c059a3b0d6386ec44c71e481f43c577124282ac
GunterWi <boi09456@gmail.com>
2024-04-16 15:46:02 +0700

    Cuoi Ky

---
+++ restaurant_cuoiky/src/main/java/com/nqt/restaurant_cuoiky/RestaurantCuoikyApplication.java
@@ -0,0 +1,13 @@
+package com.nqt.restaurant_cuoiky;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurantCuoikyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestaurantCuoikyApplication.class, args);
+	}
+
+}
---
+++ restaurant_cuoiky/src/test/java/com/nqt/restaurant_cuoiky/RestaurantCuoikyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nqt.restaurant_cuoiky;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantCuoikyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
