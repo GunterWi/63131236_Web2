4cc33a332434f124dec84187406fc892902be1c6
GunterWi <boi09456@gmail.com>
2024-05-30 03:36:41 +0700

    upload order

--- ShopThoiTrang_CuoiKy/src/main/java/com/ngt/cuoiky/controller/OrderController.java
+++ ShopThoiTrang_CuoiKy/src/main/java/com/ngt/cuoiky/controller/OrderController.java
@@ -15,4 +15,21 @@ public class OrderController {
     public String listOrderAdminFirstPage() {
         return "redirect:/admin/order/page/1";
     }
+
+    @GetMapping("/admin/order/page/{pageNum}")
+    public String listOrderAdminPage(Model model){
+        // giả định
+        model.addAttribute("keyword", keyword);
+
+        model.addAttribute("orderStatusList", orderStatusList);
+        model.addAttribute("user", user);
+        model.addAttribute("totalPages", page.getTotalPages());
+        model.addAttribute("totalItems", page.getTotalElements());
+        model.addAttribute("currentPage", pageNum);
+        model.addAttribute("listOrders", listOrders);
+        model.addAttribute("status", status);
+        model.addAttribute("startCount", startCount);
+        model.addAttribute("endCount", endCount);
+        return "order/orders";
+    }
 }
